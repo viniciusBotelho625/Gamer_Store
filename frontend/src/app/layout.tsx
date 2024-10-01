f55129dc129e21974921364f39e5c380bc6704ef
vbotelhog <vbotelhog@minsait.com>
2024-10-01 13:08:45 -0300

    Initial Config

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Montserrat } from 'next/font/google';
+
+const font = Montserrat({
+  subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+  title: "Gam3r.store",
+  description: "Full version Gam3r.Store ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={font.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
